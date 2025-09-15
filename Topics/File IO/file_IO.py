aa4079e13665fc5bbc95548d29f174f2b9df15a6
AnuJain62 <anujain.aj021@gmail.com>
2025-09-15 18:05:48 +0530

    python

--- Topics/File IO/file_IO.py
+++ Topics/File IO/file_IO.py
@@ -9,11 +9,64 @@
 # data = f.read()
 # f.close()
 
+# character           meaning
+#    'r'         open for reading(default)
+#    'w'         open for writing, truncatin the file first(overwrite the content of the file)
+#    'x'         create a new file anf open it for writing
+#    'a'         open for writing, appending to the end of the file if it exixts
+#    'b'         binary mode 
+#    't'         text mode(default) 
+#    '+'         open a disk file for uploading(reading and writing)
 
 
 
 
 
+
+# # reading to a file
+# f = open("D:\Coding\python\Topics\File IO\demo.txt" , "r")
+
+# # read() -> it reads entire file, we can pass arguments for reading number of characters from the starting
+# # data = f.read()
+# # data = f.read(5)
+
+# # readline() -> reads one line at a time 
+# data = f.readline()
+
+# print(data)
+# f.close()
+
+
+
+
+
+
+
+# writing to a file
+# 1) overwrites the entire file
+# f = open("D:\Coding\python\Topics\File IO\demo.txt","w")
+# f.write("i want to learn JS tomorrow.")
+# f.close()
+
+# 2) adds to the file
+# f = open("D:\Coding\python\Topics\File IO\demo.txt","a")
+# f.write("\nhello, I am anu jain")
+# f.close()
+
+# if file does not exist, so it is create the new file and open it for reading or writing
+# f = open("sample.txt","w")
+# f.write("It is created for writing operation of file I/O")
+# f.close()
+
+
+
+
+
+
+f = open("D:\Coding\python\Topics\File IO\demo.txt","r+")
+f.write("abc def ghi")
+print(f.read())
+f.close()
 
 
 
