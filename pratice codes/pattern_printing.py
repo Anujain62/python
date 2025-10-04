6cc7821c595e78711ac5f55374dd2d082d514941
AnuJain62 <anujain.aj021@gmail.com>
2025-10-04 23:05:30 +0530

    python

--- pratice codes/pattern_printing.py
+++ pratice codes/pattern_printing.py
@@ -32,13 +32,29 @@ n = int(input("Enter number of line :"))
 
 
 
-# 3) *
-#    **
-#    ***
-#    ****
-for i in range(0,n,1):
- for j in range(0,i+1,1):
+# # 3) *
+# #    **
+# #    ***
+# #    ****
+# for i in range(0,n,1):
+#  for j in range(0,i+1,1):
+#   print("*",end="")
+#  print() 
+
+
+
+
+
+
+
+
+# 4)     *
+#       **
+#      ***
+#     ****
+for i in range(0,n+1,1):
+ for j in range(0,n-i,1):
+  print(" ",end="")
+ for j in range(0,i,1):
   print("*",end="")
- print() 
-
-
+ print()  
