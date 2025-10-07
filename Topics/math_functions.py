206b1c812d11e14e517a4c5f14b13dc392f46aff
AnuJain62 <anujain.aj021@gmail.com>
2025-10-07 23:02:31 +0530

    python

---
+++ Topics/math_functions.py
@@ -0,0 +1,21 @@
+import math
+n = float(input("enter a number : "))
+
+
+square = math.sqrt(n)
+print("square : ",square)
+
+floor = math.floor(n)
+print("floor : ",floor)
+
+ceil = math.ceil(n)
+print("Ceil : ",ceil)
+
+power = math.pow(n,3)
+print(f"{n} to the power 3 : ",power)
+
+pi = math.pi
+print("PI : ",pi)
+
+exponantial = math.e
+print("exponantial : ",exponantial)
