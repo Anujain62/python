fed9680fedf00cda6a2dbd9b86e4cf7b5aaa2708
AnuJain62 <anujain.aj021@gmail.com>
2025-10-07 23:07:27 +0530

    python

--- pratice codes/pattern_printing.py
+++ pratice codes/pattern_printing.py
@@ -97,6 +97,22 @@ n = int(input("Enter number of line :"))
 
 
 
+# 7)   ****
+#      *  *
+#      *  *
+#      ****
+for i in range(0,n,1):
+ if(i==0 or i==n-1):
+  for j in range(0,n,1):
+   print("*",end="")
+ else:
+  print("*",end="")
+  for j in range(0,n-2,1):
+   print(" ",end="")
+  print("*",end="") 
+ print()    
+
+
 
 
 
